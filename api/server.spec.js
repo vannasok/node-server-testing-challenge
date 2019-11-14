3e4a2fcf0da32b64ff3514fc3e82e5f1f363cbdd
vanna sok <vannasok@vannas-MBP.fios-router.home>
2019-11-14 14:42:20 -0800

    test server spec

---
+++ api/server.spec.js
@@ -0,0 +1,27 @@
+const server = require('./server');
+const request = require('supertest'); // npm i supertest -D
+require('dotenv').config();
+
+describe('server', () => {
+	it('dv env set to server', () => {
+		expect(process.env.DB_ENV).toBe('testing');
+	});
+
+	describe('GET /', () => {
+		it('should return 200', () => {
+			return request(server)
+				.get('/')
+				.then(res => {
+					expect(res.status).toBe(200);
+				});
+		});
+
+		it('should return json', () => {
+			return request(server)
+				.get('/api/pets')
+				.then(res => {
+					expect(res.type).toMatch(/json/);
+				});
+		});
+	});
+});
