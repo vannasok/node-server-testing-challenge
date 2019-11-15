115e535b9269cd46d81c846da97db78c374a1403
vanna sok <vannasok@vannas-MBP.fios-router.home>
2019-11-14 16:29:50 -0800

    add cleanup seed

--- data/seeds/001-pets.js
+++ data/seeds/001-pets.js
@@ -1,13 +1,7 @@
 exports.seed = function(knex) {
-	// Deletes ALL existing entries
-	return knex('pets')
-		.truncate()
-		.then(function() {
-			// Inserts seed entries
-			return knex('pets').insert([
-				{ name: 'Ama', breed: 'cat' },
-				{ name: 'Nana', breed: 'dog' },
-				{ name: 'Da G', breed: 'husky' }
-			]);
-		});
+	return knex('pets').insert([
+		{ name: 'Ama', breed: 'cat' },
+		{ name: 'Nana', breed: 'dog' },
+		{ name: 'Da G', breed: 'husky' }
+	]);
 };
